d3866a7b669141da13600fc000248b094276545f
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-03-09 10:50:12 +0900

    Add files via upload

---
+++ Python/20210413.py
@@ -0,0 +1,22 @@
+'''
+sum = 0
+m = 1
+while m < 200:
+    sum = sum + m
+    m =  m + 2
+    
+print(sum)
+
+////
+nums = [99,59,40,70,90,78,46]
+x = 0
+max = nums[0]
+
+while x < 6:
+    x = x + 1
+    if max < nums[x]:
+       max = nums[x]
+
+print(max)
+
+'''
