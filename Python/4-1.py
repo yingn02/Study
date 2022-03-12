8de45539a17f77f129695fcb02b799a07b93e296
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-03-12 11:28:34 +0900

    Add files via upload

---
+++ Python/4-1.py
@@ -0,0 +1,15 @@
+'''
+r=float(input("반지름 입력->"))
+area=3.14*r**2
+around=2*3.14*r
+
+print("원의 넓이->",area, "원의 둘레->",around)
+
+'''
+
+a=float(input("가로길이 입력->"))
+b=float(input("세로길이 입력->"))
+
+c=a*b
+
+print("사각형의 넓이->",c)
