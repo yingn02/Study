fc7767c197a13597d0a366efe52ec20354d75f49
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-03-22 12:32:54 +0900

    Add files via upload

---
+++ Python/5-1.py
@@ -0,0 +1,16 @@
+'''
+color_list = ["red","green","blue","yellow","white"]
+
+
+print(color_list[:])
+print(color_list[1])
+print(color_list[2])
+print(color_list[2:4])
+
+'''
+color_list = []
+
+while True:
+    color = input("좋아하는 색깔을 입력하세요")
+    color_list.append(color)
+    print(color_list)
