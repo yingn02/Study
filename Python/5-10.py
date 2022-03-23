88c30d9234b751c712f12ee8e1b099c07cca9015
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-03-23 11:42:20 +0900

    Add files via upload

---
+++ Python/5-10.py
@@ -0,0 +1,20 @@
+#customer = {'name':'홍길동', 'job':'학생', '번호':'12'}
+
+'''
+customer= {}
+
+customer['name'] = '홍길동'
+customer['job'] = '학생'
+customer['num'] = '12번'
+
+print(customer)
+
+'''
+
+food = {}
+
+food['삼각김밥'] = '15개'
+food['사이다'] = '3캔' 
+food['도시락'] = '7개'
+
+print(food)
