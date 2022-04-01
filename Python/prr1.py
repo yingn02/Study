fc07f48f1bb46bf4d7e505c73ae68478b8251a2e
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-04-01 11:39:12 +0900

    Add files via upload

---
+++ Python/prr1.py
@@ -0,0 +1,11 @@
+num = 4
+sort = "사이다"
+print("오늘 %d잔의 %s를 마셨다"%(num, sort))
+
+#변수명 친구 이름이 나오도록
+
+name = "홍길동"
+print("오늘 %d잔의 %s를 %s(와)과 마셨다"%(num, sort, name))
+num = 4
+sort = "사이다"
+print(f"오늘 {num}잔의 {sort}를 마셨다")
