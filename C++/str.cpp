56064a008c6c590cbbca83c2489820998d4822c5
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-05-26 06:41:32 +0900

    Add files via upload

---
+++ C++/str.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string uni("university");
+	string name[5]; //문자열 배열 선언
+
+	name[0] = "kim";
+	name[1] = "lee";
+	name[2] = "park";
+	name[3] = "choi";
+	name[4] = "jung";
+
+	return 0;
+}
