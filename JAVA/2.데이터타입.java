4b21c2c613ae796356bc9a52899ffa3ebc0ca719
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-06-26 21:11:03 +0900

    Add files via upload

---
+++ JAVA/2.데이터타입.java
@@ -0,0 +1,12 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println(6); // Number 6
+		System.out.println("6"); // String 6
+		System.out.println("six"); // String
+		System.out.println(6 + 6); // 12
+		System.out.println("6" + "6"); //66
+		System.out.println("1111".length()); // 4
+		
+		
+	}
+}
