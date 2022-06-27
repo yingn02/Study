bb45f467073ca2792d99cd035563174da682b1cc
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-06-27 19:11:17 +0900

    Add files via upload

---
+++ JAVA/3.숫자와연산.java
@@ -0,0 +1,11 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI); // 3.14......
+		System.out.println(Math.floor(Math.PI)); // 3
+	}
+}
