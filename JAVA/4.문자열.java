6ef6be895354a738091ed68b527fff4b9d2cb18d
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-06-28 22:04:11 +0900

    Add files via upload

---
+++ JAVA/4.문자열.java
@@ -0,0 +1,10 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello"); // String
+		System.out.println("H"); // String
+		System.out.println('H'); // Character
+		
+		System.out.println("Hello\nWorld"); // 줄바꿈 \n
+		System.out.println("Hello \"World\""); // 특수기호 "
+	}
+}
