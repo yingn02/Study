c6a1c27eae07861edee11aa5991e8565ac015113
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-06-29 17:04:25 +0900

    Add files via upload

---
+++ JAVA/5.length replace.java
@@ -0,0 +1,6 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World".length()); // 11
+		System.out.println("안녕하세요 선생님 안녕히계세요 선생님".replace("선생님", "교장선생님"));
+	}
+}
