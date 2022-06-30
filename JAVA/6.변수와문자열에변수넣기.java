0f34b68339000086d28f3c72a6c7e183f84d87ad
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-06-30 17:23:49 +0900

    Add files via upload

---
+++ JAVA/6.변수와문자열에변수넣기.java
@@ -0,0 +1,18 @@
+public class hello {
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println(a); // 정수
+		
+		double b = 1.1;
+		System.out.println(b); // 실수
+		
+		String c = "Hello";
+		System.out.println(c); // 문자열
+		
+		String d = "선생님";
+		System.out.println("Hello, "+d+" Bye, "+d+"");
+		
+		
+		
+	}
+}
