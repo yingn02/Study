8ff6198579daa8926dc293260ee8d01de19c5483
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-07-01 21:57:50 +0900

    Add files via upload

---
+++ JAVA/7.데이터타입변환.java
@@ -0,0 +1,12 @@
+public class hello {
+	public static void main(String[] args) {
+		int a = (int)1.1;
+		System.out.println(a); // 1
+		
+		double b = (double)1;
+		System.out.println(b);
+		
+		String c = Integer.toString(123); // 123
+		System.out.println(c); // "123"
+	}
+}
