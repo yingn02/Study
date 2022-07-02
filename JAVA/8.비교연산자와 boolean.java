44eff0bdcd7ae383f9095a3deb2c18231ccc1a7b
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-07-02 18:42:30 +0900

    Add files via upload

---
+++ JAVA/8.비교연산자와 boolean.java
@@ -0,0 +1,10 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println(1 > 1); // False
+		System.out.println(1 < 1); // False
+		System.out.println(1 >= 1); // True
+		System.out.println(1 <= 1); // True
+		System.out.println(1 == 1); // True
+		
+	}
+}
