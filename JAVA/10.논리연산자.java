44507d3090aa8bd4b4e5fd0420e8c9c83ea06ce5
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-07-04 23:57:35 +0900

    Add files via upload

---
+++ JAVA/10.논리연산자.java
@@ -0,0 +1,8 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println(true && true); // AND
+		System.out.println(false || false); // OR
+		System.out.println(!true); // NOT
+		
+	}
+}
