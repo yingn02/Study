378cc6038da3e89928b3b148b367850d5b5588cb
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-07-05 17:40:54 +0900

    Add files via upload

---
+++ JAVA/10.반복문.java
@@ -0,0 +1,17 @@
+public class hello {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		// 0 1 2 3 4
+		while(i < 5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		// 0 1 2 3 4
+		for(i = 0; i < 5 ; i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
