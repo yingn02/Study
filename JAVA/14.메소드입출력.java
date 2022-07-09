94be26216aae287f71b941b9a437442e8b786f14
Jeong Daeun <86876440+yingn02@users.noreply.github.com>
2022-07-09 17:49:05 +0900

    Add files via upload

---
+++ JAVA/14.메소드입출력.java
@@ -0,0 +1,10 @@
+public class hello {
+	//메소드 선언
+	public static double area(int d, int h) {
+		return(d * h);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(area(5, 9)); // 45.0
+	} 
+}
